42418f0ba4b2278c6250638fe4970eed476938fd
Sameer shrinath <sameershrinath@gmail.com>
2026-04-11 17:30:48 +0530

    privacy page working fine AG

--- replace.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const index = fs.readFileSync('index.html', 'utf8');
-const headerMatch = index.match(/<header class="ic-navbar[\s\S]*?<\/header>/);
-const dashboardOld = fs.readFileSync('dashboard.html', 'utf8');
-const newDashboard = dashboardOld.replace(/<nav class="dash-nav">[\s\S]*?<\/nav>/, headerMatch[0]);
-fs.writeFileSync('dashboard.html', newDashboard);
-console.log("Done");
